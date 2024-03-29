c54aeb3c0d9bcdead20a9d5daf82a18e7e890e2f
essarebano <essarebano.dev@gmail.com>
2024-03-29 23:48:02 +0800

    Implemented mark all and remove all to form

--- src/container/TodoForm.tsx
+++ src/container/TodoForm.tsx
@@ -3,13 +3,25 @@ import { TodoContext } from "../context"
 import { TodoForm as Form } from "../components"
 
 const TodoForm = () => {
-  const { addTodo } = useContext(TodoContext)
+  const { addTodo, markAll, removeAll } = useContext(TodoContext)
 
   function onAddTodo(text?: string) {
     addTodo(text)
   }
 
-  return <Form onSubmit={onAddTodo}/>
+  function onMarkAllAsCompleted() {
+    markAll()
+  }
+
+  function onRemoveAll() {
+    removeAll()
+  }
+
+  return <Form
+    onSubmit={onAddTodo}
+    onMarkAllAsCompleted={onMarkAllAsCompleted}
+    onRemoveAll={onRemoveAll}
+  />
 }
 
 export default TodoForm
