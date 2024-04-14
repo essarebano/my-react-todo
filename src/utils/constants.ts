2ab9483c6cd721619fa8a6e2f09c21c4fdaee30e
essarebano <essarebano.dev@gmail.com>
2024-04-15 03:09:51 +0800

    Update typo on TEXT.DEFAULT

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,7 +8,7 @@ export const COLORS = {
 
 export const TEXT = {
   LOREM_IPSUM: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  DEFAULT: 'Please updated task description.'
+  DEFAULT: 'Please update task description.'
 }
 
 export const TODO_STATUS = [
