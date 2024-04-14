5aacf19cf93056e6f4bce3d41fef9d53a7fdffe8
essarebano <essarebano.dev@gmail.com>
2024-04-15 02:22:44 +0800

    Created TodoAction component

---
+++ src/components/TodoActions.tsx
@@ -0,0 +1,41 @@
+export interface TodoActionsProps {
+  onMarkAllAsCompleted: () => void
+  onRemoveAll: () => void
+  disabled: boolean
+}
+
+const TodoActions = ({
+  onMarkAllAsCompleted,
+  onRemoveAll,
+  disabled
+}: TodoActionsProps) => {
+
+  function handleOnMarkAllAsCompleted(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault()
+    onMarkAllAsCompleted()
+  }
+
+  function handleOnRemoveAll(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault()
+    onRemoveAll()
+  }
+
+  console.log({
+    disabled
+  })
+
+  return (
+    <div>
+      <button
+        onClick={handleOnMarkAllAsCompleted}
+        disabled={disabled}
+      >Mark All As Completed</button>
+      <button
+        onClick={handleOnRemoveAll}
+        disabled={disabled}
+      >Remove All</button>
+    </div>
+  )
+}
+
+export default TodoActions
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,10 @@ import TodoAddForm from "./TodoAddForm";
 import TodoItem from "./TodoItem";
 import TodoList from "./TodoList";
 import TodoCounts from "./TodoCounts"
+import TodoActions from "./TodoActions";
 
 export {
+  TodoActions,
   TodoAddForm,
   TodoItem,
   TodoList,
